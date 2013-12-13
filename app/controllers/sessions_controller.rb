2eccfd5b4d4779eb7370a07dad3b83fb95df0d0d
Todd Hainsworth <hainsworth.todd@gmail.com>
2013-12-13 17:26:23 +1030

    with has_secure_password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,13 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: user_params[:username])
 
-    if @user.password == user_params[:password]
+    if @user && @user.authenticate(user_params[:password])
       session[:user_id] = @user.id
-      redirect_to root_url
+      redirect_to root_path
     else
       render action: :new
     end
-  end
+ end
 
   def destroy
     session[:user_id] = nil
