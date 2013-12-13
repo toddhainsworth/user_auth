ea29546c341e3a30bc72976271c42ce504df07a7
Todd Hainsworth <hainsworth.todd@gmail.com>
2013-12-13 17:01:53 +1030

    auth without encrypt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: user_params[:username])
+
+    if @user.password == user_params[:password]
+      session[:user_id] = @user.id
+      redirect_to root_url
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:username, :password)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserAuth::Application.config.secret_key_base = '1d21be36b78e7d94bddf871c3ddf963c799ed62eac593f84499e4618afbe157bae2e4b960016d7884a3291be0aa1962013bce6f9c3ac6d8d023a4d08b20b604c'
